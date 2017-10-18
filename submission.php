524c4cecbbe5d1fb1c5a3d10fa8a8f0b81eef95a
jonathaneyster <jonathaneyster@gmail.com>
2017-10-18 15:43:03 -0400

    added connection string

--- submission.php
+++ submission.php
@@ -1,5 +1,35 @@
 <?php
 
+$connectstr_dbhost = '';
+$connectstr_dbname = '';
+$connectstr_dbusername = 'root';
+$connectstr_dbpassword = 'Jonathan5';
+
+foreach ($_SERVER as $key => $value) {
+    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+        continue;
+    }
+    
+    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
+}
+
+$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
+
+if (!$link) {
+    echo "Error: Unable to connect to MySQL." . PHP_EOL;
+    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+mysqli_close($link);
+
 define('DB_NAME', 'Students(ESSTestSite)');
 define('DB_USER', 'root');
 define('DB_PASSWORD', 'Jonathan5');
