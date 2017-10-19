ce784475b34b8e0d4b5d7ca927bbda8e92a0f81f
jonathaneyster <jonathaneyster@gmail.com>
2017-10-19 10:34:47 -0400

    reset connection string

--- submission.php
+++ submission.php
@@ -1,51 +1,51 @@
 <?php
 
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
+// $connectstr_dbhost = '';
+// $connectstr_dbname = '';
+// $connectstr_dbusername = '';
+// $connectstr_dbpassword = '';
 
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
+// foreach ($_SERVER as $key => $value) {
+//     if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
+//         continue;
+//     }
     
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
-
-// define('DB_NAME', 'Students(ESSTestSite)');
-// define('DB_USER', 'root');
-// define('DB_PASSWORD', 'Jonathan5');
-// define('DB_HOST', 'localhost');
-
-// $link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
-
-// if(!$link){
-// 	die('Could not connect: ' . mysql_error());
+//     $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
+//     $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
+//     $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
+//     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 // }
 
-// $db_selected = mysql_select_db(DB_NAME, $link);
+// $link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
 
-// if(!$db_selected){
-// 	die('Could not use ' . DB_NAME . ': ' . mysql_error());
+// if (!$link) {
+//     echo "Error: Unable to connect to MySQL." . PHP_EOL;
+//     echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
+//     echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
+//     exit;
 // }
+
+// echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
+// echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
+
+// mysqli_close($link);
+
+define('DB_NAME', 'elevenfifty-mysqldbserver');
+define('DB_USER', 'mysqldbuser@elevenfifty-mysqldbserver');
+define('DB_PASSWORD', 'Eleven50!@1');
+define('DB_HOST', 'elevenfifty-mysqldbserver.mysql.database.azure.com');
+
+$link = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+
+if(!$link){
+	die('Could not connect: ' . mysql_error());
+}
+
+$db_selected = mysql_select_db(DB_NAME, $link);
+
+if(!$db_selected){
+	die('Could not use ' . DB_NAME . ': ' . mysql_error());
+}
 
 $value_firstname = $_POST['firstname'];
 $value_lastname = $_POST['lastname'];
